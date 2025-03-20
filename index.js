b50c33d72011bed8b5ad5222871f66a7c0fc332b
Youngpuffy <levimuturi2@gmail.com>
2025-03-20 16:32:57 +0300

    using destructive assignment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa,oink, cluck] = farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink] =farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow,green,blue,indigo,violet]=colors
+const [r,o,y,g,b,,v]=colors
+const [,,,,,indg]=colors
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName,color,song,job,partner}=muppet
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,7 +31,7 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
+const{album:{theMuppetMovie:{song2, song4}},nestedJob,nestedPartner}=nestedMuppet
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
